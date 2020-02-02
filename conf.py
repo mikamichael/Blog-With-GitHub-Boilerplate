e0d2b437fa29f502365149d3e1d3e2bbbdae6619
mikamichael <359911923@qq.com>
2020-02-02 11:47:49 +0800

    Update site

--- conf.py
+++ conf.py
@@ -3,43 +3,52 @@
 """
 
 # For Maverick
-site_prefix = "/Blog-With-GitHub-Boilerplate/"
+site_prefix = "/"
 source_dir = "../src/"
 build_dir = "../dist/"
 index_page_size = 10
 archives_page_size = 20
 template = {
-    "name": "Galileo",
-    "type": "local",
-    "path": "../Galileo"
+    "name": "Kepler",
+    "type": "git",
+    "url": "https://github.com/AlanDecode/Maverick-Theme-Kepler.git",
+    "branch": "latest"
 }
+#template = {
+#    "name": "Galileo",
+#    "type": "local",
+#    "path": "../Galileo"
+#}
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "mikamichael/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
+category_by_folder = True
+
+
 # 站点设置
-site_name = "我的个人博客"
+site_name = "外贸启航站"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
+author = "MIKA"
+email = "359911923@qq.com"
+author_homepage = "https://www.wangxfei.com"
+description = "外贸路上，愿我们都不再孤单。"
 key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
 language = 'zh-CN'
-external_links = [
-    {
-        "name": "Maverick",
-        "url": "https://github.com/AlanDecode/Maverick",
-        "brief": "🏄‍ Go My Own Way."
-    },
-    {
-        "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
-    }
-]
+#external_links = [
+#    {
+#        "name": "Maverick",
+#        "url": "https://github.com/AlanDecode/Maverick",
+#        "brief": "🏄‍ Go My Own Way."
+#    },
+#    {
+#        "name": "三無計劃",
+#        "url": "https://www.imalan.cn",
+#        "brief": "熊猫小A的主页。"
+#    }
+#]
 nav = [
     {
         "name": "首页",
@@ -58,23 +67,23 @@ nav = [
     }
 ]
 
-social_links = [
-    {
-        "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
-        "icon": "gi gi-twitter"
-    },
-    {
-        "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
-        "icon": "gi gi-github"
-    },
-    {
-        "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
-        "icon": "gi gi-weibo"
-    }
-]
+#social_links = [
+#    {
+#        "name": "Twitter",
+#        "url": "https://twitter.com/AlanDecode",
+#        "icon": "gi gi-twitter"
+#    },
+#    {
+#        "name": "GitHub",
+#        "url": "https://github.com/AlanDecode",
+#        "icon": "gi gi-github"
+#    },
+#    {
+#        "name": "Weibo",
+#        "url": "https://weibo.com/5245109677/",
+#        "icon": "gi gi-weibo"
+#    }
+#]
 
 head_addon = r'''
 <meta http-equiv="x-dns-prefetch-control" content="on">
